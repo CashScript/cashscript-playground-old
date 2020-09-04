5c2429e2e0ae9d11dbeac2567579135349cdddc0
Rosco Kalis <roscokalis@gmail.com>
2020-09-04 16:29:40 +0200

    Init repo from copy of ashwamegh/react-typescript-markdown-editor

---
+++ src/App.tsx
@@ -0,0 +1,31 @@
+import React from 'react'
+import { css, jsx } from '@emotion/core'
+
+// Components
+import Header from './components/Header'
+import Main from './components/Main'
+import Footer from './components/Footer';
+import useDarkMode from './userDarkMode';
+
+function App() {
+  const { theme, toggleTheme } = useDarkMode();
+  const themeStyles = theme === 'light'? {
+    backgroundColor: '#eee',
+    color: '#000'
+  }: {
+    backgroundColor: '#171616',
+    color: '#fff'
+  }
+  return (
+    <div 
+      className="App"
+      style={themeStyles}
+      >
+      <Header theme={theme} toggleTheme={toggleTheme}/>
+      <Main theme={theme}/>
+      <Footer />
+    </div>
+  );
+}
+
+export default App;
---
+++ src/components/Header.tsx
@@ -0,0 +1,54 @@
+import React from 'react'
+import { FiSun } from 'react-icons/fi'
+import { FaMoon } from 'react-icons/fa'
+
+// this comment tells babel to convert jsx to calls to a function called jsx instead of React.createElement
+/** @jsx jsx */
+import { css, jsx } from '@emotion/core'
+
+interface Props {
+  toggleTheme: () => void,
+  theme: string
+}
+
+const Header: React.FC<Props> = ({ theme, toggleTheme }) => {
+
+  return (
+    <header
+      css={theme === 'dark' ?
+      css`
+        display: flex;
+        flex-direction: row;
+        justify-content: space-between;
+        background-color: #f89541;
+        padding: 24px 32px;
+        font-size: 16px;
+      `:css`
+        display: flex;
+        flex-direction: row;
+        justify-content: space-between;
+        background-color: #f8f541;
+        padding: 24px 32px;
+        box-shadow: 0px -2px 8px #000;
+        font-size: 16px;
+    `}>
+      <div className="header-title">
+        Markdown Editor
+      </div>
+      <div css={
+        css`
+          cursor: pointer;
+        `}
+        onClick={toggleTheme}
+      >
+       {
+         theme === 'dark'?
+         <FaMoon />:
+         <FiSun />
+       }
+      </div>
+    </header>
+  )
+}
+
+export default Header;
